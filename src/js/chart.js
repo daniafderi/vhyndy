0ad3ae1b6330fa16bfa3ac96a3eebee851fe24cd
bondeto <ahmadzainmuzaki@gmail.com>
2023-06-06 23:44:09 +0700

    project pbl

---
+++ src/js/chart.js
@@ -0,0 +1,59 @@
+var ctx = document.getElementById("myChart").getContext('2d');
+		var myChart = new Chart(ctx, {
+			type: 'line',
+			data: {
+				labels: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
+				datasets: [{
+					label: 'Income',
+					data: [100000, 250000, 500000, 750000, 1000000, 300000, 560000],
+					backgroundColor: [
+					'rgba(255, 99, 132, 0.2)',
+					'rgba(54, 162, 235, 0.2)',
+					'rgba(255, 206, 86, 0.2)',
+					'rgba(75, 192, 192, 0.2)',
+					'rgba(153, 102, 255, 0.2)',
+					'rgba(255, 159, 64, 0.2)'
+					],
+					borderColor: [
+					'rgba(255,99,132,1)',
+					'rgba(54, 162, 235, 1)',
+					'rgba(255, 206, 86, 1)',
+					'rgba(75, 192, 192, 1)',
+					'rgba(153, 102, 255, 1)',
+					'rgba(255, 159, 64, 1)'
+					],
+					borderWidth: 1,
+                    //pointHitRadius: [12]
+				}, {
+                    label: 'out',
+                    data: [200000, 200000, 400000, 1300000, 170000, 300000, 770000],
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 159, 64, 0.2)'
+                        ],
+                        borderColor: [
+                        'rgba(255,99,132,1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)'
+                        ],
+                        borderWidth: 1
+                }],
+                title: [100, 200, 300]
+			},
+			options: {
+				scales: {
+					yAxes: [{
+						ticks: {
+							beginAtZero:true
+						}
+					}]
+				}
+			}
+		});
---
+++ src/js/index.js
@@ -0,0 +1,26 @@
+/* 
+NOTES:
+to make this asynchronous test work, you need to change parameters in the HTML file. 
+take a look at click listener on the button, you will see "babypack".
+change it to "starterpack". 
+done!
+*/
+
+console.log('javascript ready...')
+
+// Responsive Navigation BAR 🎉
+var showNav = () => {
+    var elem = document.getElementById("header");
+    elem.classList.toggle("show");
+}
+
+var button = document.getElementById("button-mobile");
+button.addEventListener("click", showNav);
+
+// Wrapper Setting
+var wrapper = document.getElementById("outer-wrapper");
+var getHeightWrapper = wrapper.scrollHeight;
+
+if(getHeightWrapper >= outerHeight) {
+    wrapper.classList.add("relative");
+}
